6e58f94ef256b8738eccaebf1b593d1a97c6511e
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-02-16 22:09:59 -0600

    Connect JawsDb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 const mysql      = require('mysql');
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'burgers_db'
-});
- 
+let connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'burgers_db'
+  });
+}
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected to mysql on thread ${connection.threadId}`)
